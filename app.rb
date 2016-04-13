3a5de006af8f1e95807231e1cc89c27453521e9d
Frederick Silva <fredericksilva@outlook.com>
2016-04-13 18:16:10 +0000

    conteudo plano diretor

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ get '/' do
   send_file 'public/index.html' 
 end
 
+get '/titulo1' do
+    send_file 'public/mockup.html'
+end
+
 get '/stylesheets/:name.css' do
  content_type 'text/css', :charset => 'utf-8'
  scss(:"stylesheets/#{params[:name]}")
