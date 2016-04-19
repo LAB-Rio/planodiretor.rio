f5eda9e902215abab9a1ec2d0b0d159edd35eeff
Frederick Silva <fredericksilva@outlook.com>
2016-04-19 15:15:33 +0000

    add urls

--- app.rb
+++ app.rb
@@ -10,6 +10,18 @@ get '/titulo1' do
     send_file 'public/mockup.html'
 end
 
+get '/sobre' do
+    send_file 'public/plano-diretor.html'
+end
+
+get '/avaliacao'
+    send_file 'public/avaliacao.html'
+end
+
+get '/acompanhamento'
+    send_file 'public/acompanhamento.html'
+end
+
 get '/stylesheets/:name.css' do
  content_type 'text/css', :charset => 'utf-8'
  scss(:"stylesheets/#{params[:name]}")
